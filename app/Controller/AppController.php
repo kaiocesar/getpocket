64c575bc84d623bde2cc8046d1428116f7bfdb86
Kaio Cesar <tecnico.kaio@gmail.com>
2015-02-28 16:05:58 -0300

    [add] initial commit'

---
+++ app/Controller/AppController.php
@@ -0,0 +1,24 @@
+<?php
+
+App::uses('Controller', 'Controller');
+
+/**
+ * Application Controller
+ *
+ * Add your application-wide methods in the class below, your controllers
+ * will inherit them.
+ *
+ * @package		app.Controller
+ * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
+ */
+
+class AppController extends Controller 
+{
+	
+	public function beforeRender()
+	{
+		parent::beforeRender();
+		$this->layout = 'custom';
+	}
+
+}
---
+++ app/View/Pages/home.ctp
@@ -0,0 +1,3 @@
+<h1>Bem vindo ao GetCake</h1>
+<p>Grave seus links favoritos e acesse de qualquer lugar.</p>
+
